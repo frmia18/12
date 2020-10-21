b2f68cf033f6bde00aeda4118882bc28e9b07509
frmia18 <50294841+frmia18@users.noreply.github.com>
2020-10-21 08:38:20 +0700

    Add files via upload

---
+++ PBO210116415Latihan2Deklarasi.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210116415latihan2deklarasi;
+
+/**
+ *
+ * @author Frda
+ */
+public class PBO210116415Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     // TODO code application logic here
+        
+        //deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta upercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //menampilkan hasil
+        System.out.println("");
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
